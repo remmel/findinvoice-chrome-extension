8bf3550ce8e31665bbdd2d8719b419dd605c9dd7
Remy Mellet <remy.mellet@gmail.com>
2024-06-10 14:40:43 +0200

    Avoid overwriting console.log in production

--- src/ui/no-lit-warn.js
+++ src/ui/no-lit-warn.js
@@ -1,8 +1,13 @@
-console.warn = (...args) => {
-    // remove Lit warning
-    if(args.length > 0 && typeof args[0] === 'string' && args[0].startsWith('Lit is in dev mode. Not recommended for production!')) {
-        console.log('Lit!!', ...args)
-    } else {
-        console.warn(...args)
+const isDevelopment = import.meta.env.MODE === 'development';
+
+if(isDevelopment) {
+    console.warn = (...args) => {
+        // remove Lit warning
+        if(args.length > 0 && typeof args[0] === 'string' && args[0].startsWith('Lit is in dev mode. Not recommended for production!')) {
+            console.log('Lit!!', ...args)
+        } else {
+            console.warn(...args)
+        }
     }
+
 }
